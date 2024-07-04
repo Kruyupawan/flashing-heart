2a582ed4c493ab029510614d396e26c7cf5366e8
Kruyupawan <154422873+Kruyupawan@users.noreply.github.com>
2024-07-04 11:43:15 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.House)
+    music.play(music.stringPlayable("A B C5 - - - - - ", 120), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+    	
+    }
+})
